ed7473395351f6087fb7dcfcb0bae2f26d25a53d
Hanbin Qin <harveyqin86@gmail.com>
2023-05-05 17:13:37 -0400

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+// import Upload from "./pages/Upload";
+import Home from "./pages/Home";
+import Single from "./pages/Single";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import "./style.scss"
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/post/:id",
+        element: <Single />,
+      },
+      // {
+      //   path: "/upload",
+      //   element: <Upload />,
+      // },
+    ],
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
+function App() {
+
+  return (
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,55 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/authContext";
+import Logo from "../img/logo.png";
+
+const Navbar = () => {
+  const { currentUser, logout } = useContext(AuthContext);
+
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          <Link to="/">
+            <img src={Logo} alt="" />
+          </Link>
+        </div>
+        <div className="links">
+          <Link className="link" to="/?cat=tickets">
+            <h6>Tickets</h6>
+          </Link>
+          <Link className="link" to="/?cat=attrs">
+            <h6>Attractions</h6>
+          </Link>
+          <Link className="link" to="/?cat=shows">
+            <h6>Shows</h6>
+          </Link>
+          <Link className="link" to="/?cat=store">
+            <h6>Store</h6>
+          </Link>
+          <Link className="link" to="/?cat=parking">
+            <h6>Parking</h6>
+          </Link>
+          {/* <Link className="link" to="/?cat=food">
+            <h6>FOOD</h6>
+          </Link> */}
+          <span>{currentUser?.username}</span>
+          {currentUser ? (
+            <span onClick={logout}>Logout</span>
+          ) : (
+            <Link className="link" to="/login">
+              Login
+            </Link>
+          )}
+          <span className="account">
+            <Link className="link" to="/account">
+              Account
+            </Link>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
